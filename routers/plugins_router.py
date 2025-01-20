b222e7490d6e25b2954ac593099725fac4bd1440
Ethan Goh <7086cmd@gmail.com>
2025-01-20 13:08:51 +0800

    feat: translate

--- routers/plugins_router.py
+++ routers/plugins_router.py
@@ -4,7 +4,7 @@ from bson import ObjectId
 from fastapi import APIRouter, HTTPException, Depends
 from database import db
 from pydantic import BaseModel
-
+from PyDeepLX import PyDeepLX
 from util.calculate import calculate_time
 from util.cert import check_password
 from util.get_class import get_activities_related_to_user
@@ -17,4 +17,30 @@ router = APIRouter()
 @router.get('/dictionary/oxford/{word}')
 def get_word_oxford(word: str):
     result = requests.get(f'https://api.dictionaryapi.dev/api/v2/entries/en/{word}').json()
-    return result
+    return {
+        'status': 'ok',
+        'code': 200,
+        'data': result
+    }
+
+
+class TranslateRequest(BaseModel):
+    text: str
+    target_lang: str
+
+
+@router.get('/translate/deepl')
+def translate_deepl(text: str, lang: str):
+    try:
+        result = PyDeepLX.translate(text, lang, 3)
+        return {
+            'status': 'ok',
+            'code': 200,
+            'data': result
+        }
+    except PyDeepLX.TooManyRequestsException:
+        return {
+            'status': 'error',
+            'code': 429,
+            'detail': 'Too many requests'
+        }
