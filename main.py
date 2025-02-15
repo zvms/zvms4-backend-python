c2a9464c926f3d6ea2ca2f2bf4b2a2f3a029dbc4
Zcraft_ <153985444+Zcraft3793@users.noreply.github.com>
2025-02-15 17:51:23 +0800

    Add support for netlify preview site

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://v4.zvms.site"],
+    allow_origins=["http://localhost:5173", "https://v4.zvms.site", "https://v4-netlify.zvms.site/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
