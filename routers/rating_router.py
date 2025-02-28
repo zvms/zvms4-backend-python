540d1fa7440b3b8036d0e33e7a04513ecc207898
Zcraft_ <153985444+Zcraft3793@users.noreply.github.com>
2025-02-28 17:29:23 +0800

    Remove useless code

--- routers/rating_router.py
+++ routers/rating_router.py
@@ -10,53 +10,10 @@ router = APIRouter()
 
 @router.post("")
 async def create_rating(rating: ActivityRating, user=Depends(get_current_user)):
-    if 'admin' not in user['per'] and 'department' not in user['per'] and 'rating' not in user['elg']:
-        raise HTTPException(status_code=403, detail="Forbidden")
-
-    # Find duplicated document, if exists, return bad request
-    redunants = await db.zvms.ratings.find({
-        "activity_id": rating.activity_id,
-        "user_id": rating.user_id
-    }).to_list(None)
-
-    if len(redunants) > 0:
-        raise HTTPException(status_code=400, detail="Duplicated rating")
-
-    if rating.user_id != user['id']:
-        raise HTTPException(status_code=403, detail="Permission denied")
-
-    # Create rating
-    rating = rating.model_dump()
-    await db.zvms.ratings.insert_one(rating)
-    return {
-        "status": "ok",
-        "code": 201,
-        "data": {"_id": str(rating["_id"])},
-    }
-
+    raise HTTPException(status_code=410, detail="Gone")
 
 @router.get("")
 async def get_ratings(
     page: int = 1, perpage: int = 10, user=Depends(get_current_user)
 ):
-    if "admin" not in user["per"] or "rating" not in user["elg"]:
-        raise HTTPException(status_code=403, detail="Permission denied")
-    # Get ratings
-    count = await db.zvms.ratings.count_documents({})
-    result = (
-        await db.zvms.ratings.find()
-        .sort("_id", -1)
-        .skip(0 if page == -1 else (page - 1) * perpage)
-        .limit(0 if page == -1 else perpage)
-        .to_list(None if page == -1 else perpage)
-    )
-    for i in result:
-        i["_id"] = str(i["_id"])
-    return {
-        "status": "ok",
-        "code": 200,
-        "data": result,
-        "metadata": {
-            "size": count,
-        },
-    }
+    raise HTTPException(status_code=410, detail="Gone")
