3d80859bc87b4706407850575da65caf84f3f402
Zcraft_ <153985444+Zcraft3793@users.noreply.github.com>
2025-03-19 12:33:23 +0800

    fix crash when logging

--- util/user.py
+++ util/user.py
@@ -3,5 +3,8 @@ from utils import validate_object_id
 
 
 async def get_user_name(id: str):
-    user = await db.zvms.users.find_one({"_id": validate_object_id(id)}, {"name": True})
-    return user["name"]
+    try:
+        user = await db.zvms.users.find_one({"_id": validate_object_id(id)}, {"name": True})
+        return user["name"]
+    except Exception as e:
+        return "Unknown"
