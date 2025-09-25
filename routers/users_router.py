15fe686fed941dc50f1708249547ae210b7b8d21
Zcraft_ <153985444+Zcraft3793@users.noreply.github.com>
2025-09-26 06:39:58 +0800

    feat: search user log by clarity

--- routers/users_router.py
+++ routers/users_router.py
@@ -490,7 +490,7 @@ async def read_logs(
         {"user": user_oid}
         if query == ""
         else {
-            "$or": [{"url": {"$regex": query}}, {"data": {"$regex": query}}],
+            "$or": [{"url": {"$regex": query}}, {"data": {"$regex": query}}, {"clarity": {"$regex": query}}],
             "user": user_oid,
         }
     )
