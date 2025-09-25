4ceab6e948286813c0b6bfaf6748b9066d6dd2ed
Zcraft_ <153985444+Zcraft3793@users.noreply.github.com>
2025-09-25 17:30:30 +0800

    emergency patch

--- routers/activities_v2_router.py
+++ routers/activities_v2_router.py
@@ -498,6 +498,7 @@ async def amalgamate_activities_v2(
     :return: ID of the new amalgamated activity
     """
 
+    '''
     # Here, we only allow admin and volunteer to amalgamate activities.
     await volunteer.validate_create_permission(user, True)
 
@@ -626,7 +627,8 @@ async def amalgamate_activities_v2(
     )
     await log.insert_log()
     return JSONResponse({"_id": str(result.inserted_id)}, status_code=201)
-
+    '''
+    raise HTTPException(status_code=403, detail="Feature under maintenance")
 
 class UpdateUserRecord(BaseModel):
     duration: float
