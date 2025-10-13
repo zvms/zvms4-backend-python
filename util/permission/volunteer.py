ca80345b26360e327b0ff8dfd77811ab369cb83d
Zcraft_ <153985444+Zcraft3793@users.noreply.github.com>
2025-10-13 20:20:58 +0800

    Update volunteer.py (#76)

--- util/permission/volunteer.py
+++ util/permission/volunteer.py
@@ -16,29 +16,17 @@ async def validate_create_permission(user: dict, hybrid: bool):
 async def validate_update_permission(user: dict, activity: Activity) -> bool:
     if "admin" in user.get("perm") or "volunteer" in user.get("perm"):
         return True
-    # if "monitor" in user.get("perm"):
-    #     if activity.creator == str(user["id"]) or activity.appointee == str(user["id"]):
-    #         return True
-    #     else:
-    #         raise HTTPException(status_code=403, detail="Permission denied")
+    if "monitor" in user.get("perm"):
+        if activity.creator == str(user["id"]) or activity.appointee == str(user["id"]):
+            return True
+        else:
+            raise HTTPException(status_code=403, detail="Permission denied")
     raise HTTPException(status_code=403, detail="Permission denied")
 
 
 async def validate_check_permission(
     user: dict, activity: Activity, strict: bool = False
 ) -> bool:
-    if "admin" in user.get("perm"):
+    if "admin" in user.get("perm") or "volunteer" in user.get("perm"):
         return True
-    if (
-        activity.approver == "authority"
-        and "volunteer" in user.get("perm")
-        and not strict
-    ):
-        return True
-    if (
-        activity.approver == user["id"]
-        and "volunteer" in user.get("perm")
-        and not strict
-    ):
-        return True
-    return False
+    raise HTTPException(status_code=403, detail="Permission denied")
